4eeda0cba6e84514ea989d3eace70849bbbf38dc
chester <chester@technicalunderstanding.co.uk>
2021-09-06 17:41:37 +0100

    Add tax and charts

--- src/Calculate.fs
+++ src/Calculate.fs
@@ -5,21 +5,26 @@ open System
 open Fable.Core
 open Fable.Core.JsInterop
 
+[<Measure>] type gbp
+[<Measure>] type percent
+
 type Bonus = 
-    | Percentage of float
-    | AnnualLumpSum of float
+    | Percentage of float<percent>
+    | AnnualLumpSum of float<gbp>
 
-type PensionContribution = 
-    | Percentage of float
-    | Lump of float
+type CompanyContribution = {
+    Personal: float<percent>
+    Company: float<percent>
+    }
 
-type CompanyContribution = { Personal: float; Company: float option }
+let percentageOfAmount (amount: float<gbp>) (per: float<percent>) =
+    (per / 100.<percent>) * amount
 
 type Pension = 
     | CompanyContribution of CompanyContribution
     // | Lump of float
 
-type ResultPart = { Amount: float; Label: string } 
+type ResultPart = { Amount: float<gbp>; Label: string } 
 
 type CalculationResultPart =
     | Takehome of ResultPart
@@ -28,8 +33,30 @@ type CalculationResultPart =
     | Tax of ResultPart 
     | OtherCost of ResultPart 
 
+let calculatePension
+    (gross: float<gbp>)
+    (pensionOpt: Pension option) =
+    match pensionOpt with
+    | Some pension ->
+        match pension with
+        | CompanyContribution contribution ->
+            let personalPension = percentageOfAmount gross contribution.Personal
+            let companyPension = percentageOfAmount gross contribution.Company
+            let makeNet = 75.<percent>
+            [
+                OtherWealth {
+                    Amount = percentageOfAmount personalPension makeNet
+                    Label = "Personal pension (gross)"
+                }
+                OtherWealth {
+                    Amount = percentageOfAmount companyPension makeNet
+                    Label = "Company pension (gross)"
+                }
+            ]          
+    | None -> []
+    
 let calculateTaxAndTakehome
-    gross
+    (gross: float<gbp>)
     (pensionOpt: Pension option) =
     
     let returnNetAmount netAmount =
@@ -43,31 +70,37 @@ let calculateTaxAndTakehome
         | Some pension ->
             match pension with
             | CompanyContribution contribution ->
-                gross - contribution.Personal * 100.0 * gross
+                gross - (percentageOfAmount gross contribution.Personal)
         | None -> gross
+    printfn "After pension and bonus %f" grossForTax
     
     let taxFreeAllowance =
-        let maxLower = 12579.
+        let maxLower = 12579.<gbp>
         match grossForTax with
-        | x when x < 100000. -> maxLower  
-        | x when x > 125000. -> 0.
-        | x -> 
-            let diff = x - 100000.
-            let deduction = Convert.ToInt32(System.Math.Round(diff))
-            let amount = maxLower - float (deduction / 2)
+        | g when g < 100000.<gbp> -> maxLower  
+        | g when g > 125000.<gbp> -> 0.<gbp>
+        | g -> 
+            printfn "Need to calc tax allowance"
+            let diff = g - 100000.<gbp>
+            printfn "Difference to lower %f" diff
+            let deduction = Convert.ToInt32(System.Math.Round((diff / 1.<gbp>)))
+            printfn "Deduction %i" deduction
+            let amount = maxLower - (float (deduction / 2) * 1.<gbp>)
+            printfn "Amount %f" amount
             amount
-
+    printfn "Tax free allowance %f" taxFreeAllowance
+    
     let getIncomeBracketsAndAmounts taxFree = [
-            (0., Some taxFree, 0.0)
-            (taxFree, Some 50270., 20.0)
-            (50270., Some 150000., 40.0)
-            (150000., None, 45.0)
+            (0.<gbp>,      Some taxFree,      0.0<percent>)
+            (taxFree,      Some 50270.<gbp>,  20.0<percent>)
+            (50270.<gbp>,  Some 150000.<gbp>, 40.0<percent>)
+            (150000.<gbp>, None,              45.0<percent>)
         ]
 
     let nIBracketsAndAmounts = [
-            (0., Some 9568., 0.0)
-            (9568., Some 50270., 12.0)
-            (50270., None, 2.0)
+            (0.<gbp>,     Some 9568.<gbp>,  0.0<percent>)
+            (9568.<gbp>,  Some 50270.<gbp>, 12.0<percent>)
+            (50270.<gbp>, None,             2.0<percent>)
         ]
 
     let incomeTaxBracketsAndAmounts = getIncomeBracketsAndAmounts taxFreeAllowance
@@ -76,11 +109,11 @@ let calculateTaxAndTakehome
         let bracketGross = 
             match upperOpt with
             | None -> 
-                if grossForTax > lower then grossForTax - lower else 0.
+                if grossForTax > lower then grossForTax - lower else 0.<gbp>
             | Some upper ->
                 match grossForTax with
-                | x when x < lower -> 0.
-                | x when x > upper -> upper - lower
+                | g when g < lower -> 0.<gbp>
+                | g when g > upper -> upper - lower
                 | g -> g - lower
         (bracketGross, percentage)
 
@@ -88,7 +121,7 @@ let calculateTaxAndTakehome
         incomeTaxBracketsAndAmounts 
         |> List.map (fun tax -> 
             let (bracketGross, percentage) = splitTaxes tax
-            let amount = bracketGross * percentage
+            let amount = percentageOfAmount bracketGross percentage
             {
                 Amount = amount
                 Label = (sprintf "Income Tax @ %f" percentage)
@@ -99,44 +132,42 @@ let calculateTaxAndTakehome
         nIBracketsAndAmounts 
         |> List.map (fun tax -> 
             let (bracketGross, percentage) = splitTaxes tax
-            let amount = bracketGross * percentage
+            let amount = percentageOfAmount bracketGross percentage
             {
                 Amount = amount
                 Label = (sprintf "NI contribution @ %f" percentage)
             }
         )
     
-    let allTaxes = incomeTaxes @ nITaxes //|> List.filter (fun x -> x.Amount > 0.)
-    let totalTax = allTaxes |> List.sumBy (fun x -> x.Amount)
+    let allTaxes = incomeTaxes @ nITaxes //|> List.filter (fun g -> g.Amount > 0.)
+    let totalTax = allTaxes |> List.sumBy (fun g -> g.Amount)
 
     [
-        [
-            returnNetAmount (grossForTax - totalTax)
-        ]
+        [ returnNetAmount (grossForTax - totalTax) ]
         allTaxes |> List.map Tax
     ]
     |> List.collect id
 
 let calculatePerm
     gross
-    daysOff
-    (bonus: Bonus option)
-    (pension: Pension option) =
+    (bonusOpt: Bonus option)
+    (pensionOpt: Pension option) =
+    
+    let totalGross =
+        match bonusOpt with
+        | None -> gross
+        | Some (Bonus.Percentage bonus) -> gross + (percentageOfAmount gross bonus)
+        | Some (AnnualLumpSum bonus) -> gross + bonus
+    printfn "Total gross %f" totalGross
     
     [
-        Takehome {
-            Amount = 1.23
-            Label = "Net amount"
-        }
+        calculateTaxAndTakehome totalGross pensionOpt
+        calculatePension totalGross pensionOpt
     ]
+    |> List.collect id
 
 let calculateInside
     dayRate
     weeksWorked =
-    
-    [
-        Takehome {
-            Amount = 1.23
-            Label = "Net amount"
-        }
-    ]
+    let gross = dayRate * 5 * weeksWorked |> float
+    calculatePerm (gross * 1.<gbp>) None None
--- src/Components.fs
+++ src/Components.fs
@@ -2,26 +2,271 @@ namespace App
 
 open Feliz
 open Feliz.Router
+open Feliz.Plotly
+open Feliz.Bulma
+
+module Chart =
+    
+    type RedOrBlack = Red | Black
+
+    let renderChart bars =
+        let red1 = color.rgb(235, 66, 54)
+        let red2 = color.rgb(245, 92, 81)
+        let red3 = color.rgb(247, 137, 129)
+        let black1 = color.rgb(33, 33, 33)
+        let black2 = color.rgb(71, 70, 70)
+        let black3 = color.rgb(115, 115, 115)
+        let reds = [ red1; red2; red3 ]
+        let blacks = [ black1; black2; black3 ]
+        
+        let folder = fun ((nred, nblack), agg) ((redOrBlack: RedOrBlack), label, (values: float<gbp> list)) ->
+            let values = values |> List.map (fun x -> x / 1.0<gbp>)
+            let colourRbg = 
+                match redOrBlack with
+                | Red -> reds.[nred % 3]
+                | Black -> blacks.[nblack % 3]
+
+            let trace = traces.bar [
+                    bar.x [ "perm"; "inside" ]
+                    bar.y values
+                    bar.name label
+                    bar.marker [ marker.color colourRbg ]
+                ]
+            let nextRB =
+                match redOrBlack with
+                | Red -> (nred + 1, nblack)
+                | Black -> (nred, nblack + 1)
+
+            (nextRB, agg @ [trace])
+
+        let foldResult: (int * int) * ITracesProperty list = bars |> List.fold folder ((0, 0), [])
+        let traces = foldResult |> snd
+        
+        Plotly.plot [
+            plot.traces traces
+            plot.layout [
+                layout.barmode.stack
+            ]
+        ]
+    
+    let renderDetailed perm inside =
+        let getTakeHome results = results |> List.choose (fun x -> match x with | Takehome th -> Some th | _ -> None) |> List.exactlyOne
+        let getTaxes results = results |> List.choose (fun x -> match x with | Tax t -> Some t | _ -> None)
+        let getOtherWealth results = results |> List.choose (fun x -> match x with | OtherWealth t -> Some t | _ -> None)
+        
+        let permNet = getTakeHome perm
+        let insideNet = getTakeHome inside
+
+        let getParts p i rob filter =
+            let permParts = filter p
+            let insideParts = filter i
+            let labels = List.distinct (
+                    (List.map (fun x -> x.Label) permParts) @ (List.map (fun x -> x.Label) insideParts)
+                )
+            
+            labels
+            |> List.map (fun l -> 
+                let getValueOfLabelOrZero parts =
+                    parts
+                    |> List.filter (fun x -> x.Label = l)
+                    |> List.tryExactlyOne
+                    |> Option.map (fun x -> x.Amount)
+                    |> Option.defaultWith (fun () -> 0.0<gbp>)
+                let permValue = getValueOfLabelOrZero permParts
+                let insideValue = getValueOfLabelOrZero insideParts
+                (rob, l, [ permValue; insideValue ])
+            )
+
+        let net = [
+            (Black, permNet.Label, [ permNet.Amount; insideNet.Amount ])
+        ]
+        let taxesParts = getParts perm inside Red getTaxes
+        let otherWealthParts = getParts perm inside Black getOtherWealth
+
+        renderChart (net @ otherWealthParts @ taxesParts)
+
+    let renderSimple perm inside =
+        let getTakeHome results = results |> List.choose (fun x -> match x with | Takehome th -> Some th | _ -> None) |> List.exactlyOne
+        let getOtherWealth results = results |> List.choose (fun x -> match x with | OtherWealth t -> Some t | _ -> None)
+        let getTaxes results = results |> List.choose (fun x -> match x with | Tax t -> Some t | _ -> None)
+        let getOtherCost results = results |> List.choose (fun x -> match x with | OtherCost t -> Some t | _ -> None)
+        
+        let permBlack =
+            [ getTakeHome perm ] @ getOtherWealth perm
+            |> List.sumBy (fun x -> x.Amount)
+        
+        let permRed =
+            getTaxes perm @ getOtherCost perm
+            |> List.sumBy (fun x -> x.Amount)
+        
+        let insideBlack =
+            [ getTakeHome inside ] @ getOtherWealth inside
+            |> List.sumBy (fun x -> x.Amount)
+        
+        let insideRed =
+            getTaxes inside @ getOtherCost inside
+            |> List.sumBy (fun x -> x.Amount)
+
+        renderChart [
+            (Black, "Wealth", [permBlack; insideBlack])
+            (Red, "Tax", [permRed; insideRed])
+        ]
+
 
 type Components =
-    /// <summary>
-    /// The simplest possible React component.
-    /// Shows a header with the text Hello World
-    /// </summary>
     [<ReactComponent>]
-    static member HelloWorld() = Html.h1 "Hello World"
+    static member Home() =
+        let (grossSalary, setGrossSalary) = React.useState(45000.)
+        let (bonus, setBonus) = React.useState(10.)
+        let (pensionCompany, setPensionCompany) = React.useState(12.)
+        let (pensionPersonal, setPensionPersonal) = React.useState(8.)
 
-    /// <summary>
-    /// A stateful React component that maintains a counter
-    /// </summary>
-    [<ReactComponent>]
-    static member Counter() =
-        let (count, setCount) = React.useState(0)
+        let (dayRate, setDayRate) = React.useState(350)
+        let (numberOfWeeks, setNumberOfWeeks) = React.useState(44)
+        
+        let (showDetailed, setShowDetailed) = React.useState(false)
+
+        let insideParts = calculateInside dayRate numberOfWeeks
+        
+        let pension = CompanyContribution {
+            Personal = (pensionPersonal * 1.0<percent>)
+            Company = (pensionCompany * 1.0<percent>)
+            }
+        let permParts =
+            calculatePerm
+                (grossSalary * 1.0<gbp>)
+                (Some (Bonus.Percentage (bonus * 1.0<percent>)))
+                (Some pension)
+        
+        let grossSalaryChanged = fun (s) -> s |> setGrossSalary
+        let bonusChanged = fun (s) -> s |> setBonus
+        let pensionPersonalChanged = fun (s) -> s |> setPensionPersonal
+        let pensionCompanyChanged = fun (s) -> s |> setPensionCompany
+        
+        let colWidths = 400
+        let colHeight = 600
+
+        let ir35 =
+            Html.div [
+                prop.style [
+                    style.width colWidths
+                    //style.height colHeight
+                    style.display.inlineBlock
+                ]
+                prop.children [
+                    Bulma.card [
+                        Bulma.cardContent [
+                            Bulma.content "Inside IR35"
+                            Bulma.field.div [
+                                Bulma.label "Day rate"
+                                Bulma.control.div [
+                                    Bulma.input.number [
+                                        prop.onChange setDayRate
+                                        prop.value dayRate
+                                    ]
+                                ]
+                            ]
+                            Bulma.field.div [
+                                Bulma.label "Number of weeks"
+                                Bulma.control.div [
+                                    Bulma.input.number [
+                                        prop.onChange setNumberOfWeeks
+                                        prop.value numberOfWeeks
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+        let perm =
+            Html.div [
+                prop.style [
+                    style.width colWidths
+                    style.display.inlineBlock
+                ]
+                prop.children [
+                    Bulma.card [
+                        Bulma.cardContent [
+                            Bulma.content "Permeanent"
+                            Bulma.field.div [
+                                Bulma.label "Gross salary"
+                                Bulma.control.div [
+                                    Bulma.input.number [
+                                        prop.onChange grossSalaryChanged
+                                        prop.value grossSalary
+                                    ]
+                                ]
+                            ]
+                            Bulma.field.div [
+                                Bulma.label "Bonus"
+                                Bulma.control.div [
+                                    Bulma.input.number [
+                                        prop.onChange bonusChanged
+                                        prop.value bonus
+                                    ]
+                                ]
+                            ]
+                            Bulma.field.div [
+                                Bulma.label "Pension personal"
+                                Bulma.control.div [
+                                    Bulma.input.number [
+                                        prop.onChange pensionPersonalChanged
+                                        prop.value pensionPersonal
+                                    ]
+                                ]
+                            ]
+                            Bulma.field.div [
+                                Bulma.label "Pension company"
+                                Bulma.control.div [
+                                    Bulma.input.number [
+                                        prop.onChange pensionCompanyChanged
+                                        prop.value pensionCompany
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+
+        let chartFunc = if showDetailed then Chart.renderDetailed else Chart.renderSimple
+
         Html.div [
-            Html.h1 count
-            Html.button [
-                prop.onClick (fun _ -> setCount(count + 1))
-                prop.text "Increment"
+            Bulma.navbar [
+                Bulma.color.isPrimary
+                prop.children [
+                    Bulma.navbarBrand.div [
+                        Bulma.navbarItem.a [
+                            Html.h3 [ prop.text "Perm vs inside"; prop.height 28; prop.width 112; ]
+                        ]
+                    ]
+                ]
+            ]
+
+            ir35
+            perm
+
+            Html.div [
+                prop.children [
+                    Bulma.card [
+                        Bulma.cardContent [
+                            Bulma.field.div [
+                                Bulma.label "Show detailed"
+                                Bulma.control.div [
+                                    Bulma.input.checkbox [
+                                        prop.value showDetailed
+                                        prop.onChange setShowDetailed
+                                    ]
+                                ]
+                            ]
+                        ]
+                    ]
+                ]
+            ]
+            
+            Html.div [
+                chartFunc permParts insideParts
             ]
         ]
 
@@ -36,9 +281,9 @@ type Components =
             router.onUrlChanged updateUrl
             router.children [
                 match currentUrl with
-                | [ ] -> Html.h1 "Index"
-                | [ "hello" ] -> Components.HelloWorld()
-                | [ "counter" ] -> Components.Counter()
+                | [ ] -> Components.Home()
+                //| [ "hello" ] -> Components.HelloWorld()
+                //| [ "counter" ] -> Components.Counter()
                 | otherwise -> Html.h1 "Not found"
             ]
         ]
